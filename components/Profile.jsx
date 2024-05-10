829ecf60ec687d03d34af9b59b38a5731ea5bfa3
Yasas Waruna Weerage <it21225260@my.sliit.lk>
2024-05-10 17:01:41 +0530

    added rrr

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -23,7 +23,7 @@ const Profile = () => {
           
           setUserData(data);
         } else {
-          Alert.alert('Error', 'User not found');
+          Alert.alert('Error', 'Userrr not found');
         }
         setLoading(false);
       } catch (error) {
